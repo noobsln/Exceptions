cd4cce025747798e1f8bb24894744f0512cdeef3
noobsln <alyssamyers2020@gmail.com>
2024-04-26 00:08:35 -0400

    character function created

--- Exceptions/Source.cpp
+++ Exceptions/Source.cpp
@@ -9,3 +9,11 @@ class InvalidRangeException : public runtime_error {
 public:
 	InvalidRangeException() : runtime_error("ERR:Invalid range") {}
 };
+
+char character(char start, int offset) {
+	if (!isalpha(start) || isupper(start))
+		throw InvalidCharacterException();
+	if (!isdigit(offset) || (start + offset) < 'a' || (start + offset) > 'z')
+		throw InvalidRangeException();
+	return (start + offset);
+}
